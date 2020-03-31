de87e6f8565cd37740d66a4462bf76957b5a0680
Vani <vani-lea@hotmail.com>
2020-03-31 11:43:02 +0200

    haló

---
+++ reg_insert.php
@@ -0,0 +1,29 @@
+<?php
+    include ("include.php");
+    $db = mysqli_connect($bhost,$buser,$bpasswd);
+
+    $lastname = ($_POST['Lastname']);
+    $firstname = ($_POST['Firstname']);
+    $email = ($_POST['EMail']);
+    $address = ($_POST['Address']);
+    $phone = ($_POST['Phonenumber']);
+    $password = ($_POST['UserPassword']);
+    $password2 = ($_POST['UserPassword2']);
+
+
+	mysqli_select_db($db,'bidwebpagedatabase');
+    if ($password == $password2)
+	{
+        $password = md5($password); //hashing password
+        $sql = "INSERT into registeredusers VALUES('','$lastname','$firstname','$email','$address','$phone','$password')";
+        mysqli_query($db, $sql);
+        echo 'Sikeres regisztráció<br>';
+    }
+    else 
+        echo 'A két jelszó nem egyezik<br>';
+
+?>
+
+<FORM action="register.php">
+
+</FORM>
---
+++ register.php
@@ -0,0 +1,25 @@
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Regisztráció</title>
+</head>
+<body>
+Regisztráció<br><br>  
+<form action="reg_insert.php" method="POST">
+<table>
+<tr><input type="text" name="Lastname" placeholder="Vezetéknév"><br>
+<tr><input type="text" name="Firstname" placeholder="Keresztnév"><br>
+<tr><input type="text" name="EMail" placeholder="E-mail"><br>
+<tr><input type="text" name="Address" placeholder="Cím"><br>
+<tr><input type="text" name="Phonenumber" placeholder="Telefonszám"><br>
+<tr><input type="text" name="UserPassword" placeholder="Jelszó"><br>
+<tr><input type="text" name="UserPassword2" placeholder="Jelszó újra"><br>
+<tr><input type="submit" name="reg_btn" value="Regisztráció">   
+</table>
+</form>
+
+
+</body>
+</html>
