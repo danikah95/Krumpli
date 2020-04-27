9d638b7a4753c21c2fdcc7d147b21870b36a271a
Staub Daniel <stubi95.ds@gmail.com>
2020-04-27 11:22:12 +0200

    front end + search + insert item

--- login.php
+++ login.php
@@ -1,19 +1,88 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bejelentkezés</title>
-</head>
-<body>
-Bejelentkezés<br><br>  
-<form action="log.php" method="POST">
-<table>
-<tr><input type="text" name="EMail" placeholder="E-mail"><br>
-<tr><input type="text" name="UserPassword" placeholder="Jelszó"><br>
-<tr><input type="submit" name="log_button" value="Bejelentkezés">   
-</table>
-</form>
+<!DOCTYPE html>
+<html lang="hu">
 
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>B-Tech Vatera</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/customcss-homepage.css" rel="stylesheet">
+
+</head>
+
+
+
+<body>
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-info fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="index.php">B-Tech Vatera</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="index.php">Főoldal
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="register.php">Regisztráció</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="login.php">Bejelentkezés</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Page Content -->
+
+
+  <div class="signup-form">	
+    <form action="log.php" method="POST">
+		<h2>Bejelentkezés</h2>
+        <div class="form-group">
+			<div class="input-group">
+				<input type="email" class="form-control" name="EMail" placeholder="E-mail cím" required="required">
+			</div>
+        </div>
+
+		<div class="form-group">
+			<div class="input-group">
+				<input type="password" class="form-control" name="UserPassword" placeholder="Jelszó" required="required">
+			</div>
+        </div>
+      
+		<div class="form-group">
+            <button type="submit" class="btn btn-info btn-block btn-lg" name="log_button">Bejelentkezés</button>
+        </div>
+    </form>
+	    <div class="text-center">Még nincs fiókja? <a href="register.php">Regisztráljon most</a>.</div>
+    </div>
+
+
+    </div>
+    <!-- /.row -->
+
+  </div>
+  <!-- /.container -->
+
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
 </body>
+
 </html>
--- reg_insert.php
+++ reg_insert.php
@@ -1,5 +1,44 @@
-<?php
+<head>
+    <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/customcss-homepage.css" rel="stylesheet">
+</head>
+
+
+<body>
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="index.php">B-Tech Vatera</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="index.php">Főoldal
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="register.php">Regisztráció</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="login.php">Bejelentkezés</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Page Content -->
+
+
+  <?php
     include ("include.php");
+    include ("alert_insert.php");
     $db = mysqli_connect($bhost,$buser,$bpasswd);
 
     $lastname = ($_POST['Lastname']);
@@ -12,19 +51,30 @@
 
 
 	mysqli_select_db($db,'bidwebpagedatabase');
-    if ($password == $password2)
+  if ($password == $password2)
 	{
-        $password = md5($password); //hashing password
-        $sql = "INSERT into registeredusers VALUES('','$lastname','$firstname','$email','$address','$phone','$password','')";
-        mysqli_query($db, $sql);
-        echo "Sikeres regisztráció!";
-
-    }
-    else 
-        echo "A két jelszó nem egyezik";
-            
+    $password = md5($password); //hashing password
+    $sql = "INSERT into registeredusers VALUES('','$lastname','$firstname','$email','$address','$phone','$password','')";
+    mysqli_query($db, $sql);
+    //echo "Sikeres regisztráció!";
+		fun_alert ("Sikeres regisztráció!", "index.php");
+  }
+  else
+	{
+    //echo "A két jelszó nem egyezik";
+		fun_alert ("A két jelszó nem egyezik!", "register.php");
+	}
 
 ?>
+  <!-- /.container -->
+
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
 
 <FORM action="register.php">
 
