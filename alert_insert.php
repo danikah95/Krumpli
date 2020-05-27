f9057aff0a88fdebfe1b62bfd5eccd6719932026
Staub Daniel <stubi95.ds@gmail.com>
2020-05-27 10:50:13 +0200

    del

--- alert_insert.php
+++
@@ -1,6 +0,0 @@
-<?php
-	function fun_alert($message, $location)
-	{
-		echo "<script type='text/javascript'>alert('$message'); window.location.href='$location'</script>";
-	}
-?>
--- kijelentkezes.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include ("alert_insert.php");
-	session_start();
-	session_unset();
-	session_destroy();
-	//echo "Sikeres kijelentkezés!";
-	//header('Location: index.html');
-	fun_alert ("Sikeres kijelentkezés!", "index.php");
-?>
